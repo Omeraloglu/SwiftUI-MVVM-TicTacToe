b047896e4e4bbf516ac40f1082c3749f3c5a5c91
Omer <omeraloglu16@hotmail.com>
2023-04-29 18:17:57 +0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Ömer ALOĞLU on 29.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
